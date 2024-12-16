2b2459ab4dca62bba468c2964477b3f17e6c868c
Yaroslav Gusev <yarigusev@gmail.com>
2024-12-16 16:19:15 +0300

    add size browser

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,5 @@ from selene import browser
 @pytest.fixture(scope='session')
 def open_browser():
     browser.config.base_url = "https://github.com"
+    browser.config.window_width = 1280
+    browser.config.window_height = 720
