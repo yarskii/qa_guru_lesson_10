e6691de8c9b3e8040dd9259dbb286066c6135205
Yaroslav Gusev <yarigusev@gmail.com>
2024-12-19 17:27:31 +0300

    add .should(be.visible)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from selene import browser, be, have
+from selene import browser, be
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
